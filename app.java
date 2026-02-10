617af4d0ab9c9fe8d27736ff81695dd2093c97b7
root <root@ip-172-31-31-254.ec2.internal>
2026-02-10 10:39:28 +0000

    new data

---
+++ app.java
@@ -0,0 +1,11 @@
+class GFG {
+    public static void main(String[] args) {
+        int n = 100;
+
+        if ((n | 1) > n) {
+            System.out.println("Number is Even");
+        } else {
+            System.out.println("Number is Odd");
+        }
+    }
+}
